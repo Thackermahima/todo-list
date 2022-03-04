39c9816fcbd4d52177fc51c5b4baff54c8c49c53
Mahima <mahimathacker75@gmail.com>
2022-03-04 22:12:58 +0530

    Completed todolist

---
+++ src/Components/TodoItem.jsx
@@ -0,0 +1,17 @@
+import React , {useState} from "react";
+function TodoItem(props) {
+
+    
+    return (
+        <div onClick= {
+            () => {
+                props.onChecked(props.id)
+            }
+        }>
+            <li>{props.text}</li>
+
+        </div>
+    )
+
+}
+export default TodoItem;
